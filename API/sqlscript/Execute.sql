719ef36266f00fe42b7dd580869d62efbe41ed3f
rkishore2015 <rajkishore_rajkishore@yahoo.com>
2023-12-10 13:59:50 -0600

    end of section2

---
+++ API/sqlscript/Execute.sql
@@ -0,0 +1,4 @@
+-- SQLite
+INSERT INTO Products (Id, Name)
+VALUES (1, "Product 1"),(2, "Product 2"),(3, "Product 3"),(4, "Product 4"),(5, "Product 5"),(6, "Product 6"),(7, "Product 7"),(8, "Product 8"),(9, "Product 9"),(10, "Product 10");
+
---
+++ Infrastructure/Data/Migration/20231210181457_IntialCreate.Designer.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Data.Migration
+{
+    [DbContext(typeof(StoreContext))]
+    [Migration("20231210181457_IntialCreate")]
+    partial class IntialCreate
+    {
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
+        {
+#pragma warning disable 612, 618
+            modelBuilder.HasAnnotation("ProductVersion", "7.0.14");
+
+            modelBuilder.Entity("API.Entities.Product", b =>
+                {
+                    b.Property<int>("Id")
+                        .ValueGeneratedOnAdd()
+                        .HasColumnType("INTEGER");
+
+                    b.Property<string>("Name")
+                        .HasColumnType("TEXT");
+
+                    b.HasKey("Id");
+
+                    b.ToTable("Products");
+                });
+#pragma warning restore 612, 618
+        }
+    }
+}
